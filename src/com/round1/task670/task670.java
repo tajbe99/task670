3243405f9babd395dc624b24059740845cf2a81a
tajbe <tajbe123@gmail.com>
2018-02-04 23:34:47 +0300

    create buffer

--- src/com/round1/task670/task670.java
+++ src/com/round1/task670/task670.java
@@ -1,14 +1,37 @@
 package com.round1.task670;
 
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
+import java.io.*;
+import java.io.IOException;
 
 public class task670 {
     public static void main(String[] args){
-        BufferedReader inputFile = new FileReader("input.txt");
+        BufferedReader readBuff = null;
+        try {
+            readBuff = new BufferedReader(new FileReader("src\\com\\round1\\task670\\input.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            while (true){
+                String line = readBuff.readLine();
+                if (line==null) break;
+                try (BufferedWriter writeBuff = new BufferedWriter(new FileWriter("src\\com\\round1\\task670\\output.txt",false)))
+                {
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                readBuff.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
 
     }
 }
