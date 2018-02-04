b084fc640980510a67e6a797f65bf835991668be
tajbe <tajbe123@gmail.com>
2018-02-04 22:47:01 +0300

    Create input and output file

--- src/com/round1/task670/task670.java
+++ src/com/round1/task670/task670.java
@@ -1,4 +1,14 @@
 package com.round1.task670;
 
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+
 public class task670 {
+    public static void main(String[] args){
+        BufferedReader inputFile = new FileReader("input.txt");
+
+    }
 }
